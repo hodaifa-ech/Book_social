d9121f5382a70f8baa7958b2a8f71ea9d544729a
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-06 23:19:58 +0100

    Role Entity

--- book-network/src/main/java/com/alibou/book/user/User.java
+++ book-network/src/main/java/com/alibou/book/user/User.java
@@ -1,11 +1,13 @@
 package com.alibou.book.user;
 
+import com.alibou.book.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
@@ -13,6 +15,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 @Setter
 @Builder
@@ -35,6 +39,9 @@ public class User implements UserDetails , Principal {
     private boolean accountLocked;
     private boolean  enabled;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
     @CreatedDate
     @Column(nullable = false,updatable = false)
    private LocalDateTime createdDate;
@@ -49,7 +56,9 @@ public class User implements UserDetails , Principal {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return this.roles
+                .stream().map(r-> new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
     }
 
     @Override
