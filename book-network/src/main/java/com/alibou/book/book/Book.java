1b874b97e5ec6c8d580b5b5a12ba3c62c4eacff8
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-14 17:15:43 +0100

    entity book

--- book-network/src/main/java/com/alibou/book/book/Book.java
+++ book-network/src/main/java/com/alibou/book/book/Book.java
@@ -1,4 +1,49 @@
 package com.alibou.book.book;
 
-public class Book {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@SuperBuilder()
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book  {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    @Column(insertable = false )
+    private LocalDateTime lastModifiedDate;
+
+
+    @CreatedBy
+    @Column(nullable = false,updatable = false)
+    private Integer createdBy;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
 }
---
+++ book-network/src/main/java/com/alibou/book/feedback/Feedback.java
@@ -0,0 +1,4 @@
+package com.alibou.book.feedback;
+
+public class Feedback {
+}
