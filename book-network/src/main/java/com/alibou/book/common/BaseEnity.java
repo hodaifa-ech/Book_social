94342bcff5cb72c582785ba57c95131466679297
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-14 17:27:26 +0100

    feedback entity

---
+++ book-network/src/main/java/com/alibou/book/common/BaseEnity.java
@@ -0,0 +1,4 @@
+package com.alibou.book.common;
+
+public class BaseEnity {
+}
--- book-network/src/main/java/com/alibou/book/feedback/Feedback.java
+++ book-network/src/main/java/com/alibou/book/feedback/Feedback.java
@@ -1,4 +1,42 @@
 package com.alibou.book.feedback;
 
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Feedback {
+
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private Double note;
+
+    private String comment;
+
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    @Column(insertable = false )
+    private LocalDateTime lastModifiedDate;
+
+
+    @CreatedBy
+    @Column(nullable = false,updatable = false)
+    private Integer createdBy;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
 }
+
