380f63122feb7be2a8b5b14cb3b457c3316ba65b
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-19 13:39:26 +0100

    add save feedback

---
+++ book-network/src/main/java/com/alibou/book/feedback/FeedbackService.java
@@ -0,0 +1,43 @@
+package com.alibou.book.feedback;
+
+import com.alibou.book.book.Book;
+import com.alibou.book.book.BookRepository;
+import com.alibou.book.common.PageResponse;
+import com.alibou.book.exception.OperationNotPermittedException;
+import com.alibou.book.user.User;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackService {
+
+    private final BookRepository bookRepository;
+    private final FeedbackMapper feedbackMapper;
+    private final FeedBackRepository feedBackRepository;
+
+
+    public Integer save(FeedbackRequest request, Authentication connectedUser) {
+        Book book = bookRepository.findById(request.bookId())
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + request.bookId()));
+        if (book.isArchived() || !book.isShareable()) {
+            throw new OperationNotPermittedException("You cannot give a feedback for and archived or not shareable book");
+        }
+         User user = ((User) connectedUser.getPrincipal());
+        if (Objects.equals(book.getCreatedBy(), connectedUser.getName())) {
+            throw new OperationNotPermittedException("You cannot give feedback to your own book");
+        }
+        Feedback feedback = feedbackMapper.toFeedback(request);
+        return feedBackRepository.save(feedback).getId();
+    }
+
+}
