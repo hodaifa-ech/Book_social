e018bf80e7087f1563b50542cde4fcd6bf57f7fe
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-19 14:23:18 +0100

    add OpenApi Config

--- book-network/src/main/java/com/alibou/book/config/OpenApiConfig.java
+++ book-network/src/main/java/com/alibou/book/config/OpenApiConfig.java
@@ -10,5 +10,21 @@ import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Alibou",
+                        email = "contact@aliboucoding.com",
+                        url = "https://aliboucoding.com/course"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - Alibou",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
 public class OpenApiConfig {
 }
