d328c7f3b9290de7b3dda46a0461f6efc9680e46
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-20 15:21:15 +0100

    add openapi

--- book-network/src/main/java/com/alibou/book/history/BookTransactionHistoryRepository.java
+++ book-network/src/main/java/com/alibou/book/history/BookTransactionHistoryRepository.java
@@ -57,14 +57,12 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
 
 
     @Query("""
-                 SELECT transaction
-                FROM BookTransactionHistory  transaction
-                WHERE transaction.book.owner.id = :userId
-                AND transaction.book.id = :bookId
-                AND transaction.returned = true
-                AND transaction.returnApproved = false
-    
-    """
-    )
-    Optional<BookTransactionHistory>  findByBookIdAndOwnerId(Integer bookId, String name);
+            SELECT transaction
+            FROM BookTransactionHistory  transaction
+            WHERE transaction.book.createdBy = :userId
+            AND transaction.book.id = :bookId
+            AND transaction.returned = true
+            AND transaction.returnApproved = false
+            """)
+    Optional<BookTransactionHistory> findByBookIdAndOwnerId(@Param("bookId") Integer bookId, @Param("userId") String userId);
 }
