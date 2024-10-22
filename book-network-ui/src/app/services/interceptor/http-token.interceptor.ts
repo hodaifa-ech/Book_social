3a5a6317559ac72b8ed30f2dc01326ab081f1738
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-22 17:19:23 +0100

    add token filter

--- book-network-ui/src/app/services/interceptor/http-token.interceptor.ts
+++ book-network-ui/src/app/services/interceptor/http-token.interceptor.ts
@@ -1,5 +1,27 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {TokenService} from '../token/token.service';
+@Injectable()
+export class HttpTokenInterceptor implements HttpInterceptor {
 
-export const httpTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+  constructor(
+    private tokeService: TokenService
+  ) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.tokeService.token;
+    if(token){
+        const authReq=request.clone({
+          headers:new HttpHeaders({
+            Authorization: 'Bearer'+token
+          })
+        });
+        return next.handle(authReq);
+    }
+    return next.handle(request);
+  }
+
+
+}
