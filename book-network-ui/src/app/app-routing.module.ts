db6b5a5905159207d56a0e26fc9640a4b61ee3eb
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-22 16:04:12 +0100

    add menu

--- book-network-ui/src/app/app-routing.module.ts
+++ book-network-ui/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
 import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
+import {BookModule} from './modules/book/book.module';
 
 const routes: Routes = [
 
@@ -16,7 +17,11 @@ const routes: Routes = [
   },
   {
     path: 'activate-account',
-    component : ActivateAccountComponent
+    loadChildren:() => import('./modules/book/book.module').then(m => m.BookModule)
+  },
+  {
+    path: 'book',
+    component : BookModule
   }
 ];
 
