8ac2d84991d400685c5b3f95bc71636991602f71
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-25 14:25:26 +0100

    add guard logique

--- book-network-ui/src/app/services/guard/auth.guard.ts
+++ book-network-ui/src/app/services/guard/auth.guard.ts
@@ -1,5 +1,12 @@
-import { CanActivateFn } from '@angular/router';
-
-export const authGuard: CanActivateFn = (route, state) => {
+import {CanActivateFn, Router} from '@angular/router';
+import {TokenService} from '../token/token.service';
+import {inject} from '@angular/core';
+export const authGuard: CanActivateFn = () => {
+  const  tokenService =inject(TokenService);
+  const router=inject(Router);
+  if(tokenService.isTokenNotValid()){
+    router.navigate(['login']);
+    return false;
+  }
   return true;
 };
--- book-network-ui/src/app/services/token/token.service.ts
+++ book-network-ui/src/app/services/token/token.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +12,22 @@ export class TokenService {
       get  token(){
         return localStorage.getItem(('token')) as string;
       }
+
+  isTokenNotValid() {
+    return !this.isTokenValid();
+  }
+
+  private isTokenValid() {
+        const token=this.token;
+        if(!token){
+          return false;
+        }
+        const jwtHelper=new JwtHelperService();
+        const isTokenExpired=jwtHelper.isTokenExpired(token);
+        if(isTokenExpired){
+          localStorage.clear();
+          return false;
+        }
+    return true;
+  }
 }
