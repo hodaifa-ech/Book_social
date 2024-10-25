59c27b98362a1f42b1f10989c444229da24f174c
hodaifa-ech <echffani.hodaifa@etu.uae.ac.ma>
2024-10-25 14:28:59 +0100

    test

--- book-network-ui/src/app/app-routing.module.ts
+++ book-network-ui/src/app/app-routing.module.ts
@@ -2,26 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
+import {authGuard} from './services/guard/auth.guard';
 import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
-import {BookModule} from './modules/book/book.module';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    redirectTo: 'books',
+    pathMatch: 'full'
+  },
   {
     path: 'login',
-    component : LoginComponent
+    component: LoginComponent
   },
   {
     path: 'register',
-    component : RegisterComponent
+    component: RegisterComponent
   },
   {
     path: 'activate-account',
-    loadChildren:() => import('./modules/book/book.module').then(m => m.BookModule)
+    component: ActivateAccountComponent
   },
   {
-    path: 'book',
-    component : BookModule
+    path: 'books',
+    loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule),
+    canActivate: [authGuard]
   }
 ];
 
--- book-network-ui/src/app/modules/book/book-routing.module.ts
+++ book-network-ui/src/app/modules/book/book-routing.module.ts
@@ -1,45 +1,58 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {MainComponent} from './pages/main/main.component';
-import {BookModule} from './book.module';
 import {BookListComponent} from './pages/book-list/book-list.component';
 import {MyBooksComponent} from './pages/my-books/my-books.component';
 import {ManageBookComponent} from './pages/manage-book/manage-book.component';
 import {BorrowedBookListComponent} from './pages/borrowed-book-list/borrowed-book-list.component';
+import {ReturnedBooksComponent} from './pages/returned-books/returned-books.component';
+import {authGuard} from '../../services/guard/auth.guard';
+
 
 const routes: Routes = [
   {
-    path:'',
-    component:MainComponent,
-    children:[
+    path: '',
+    component: MainComponent,
+    canActivate: [authGuard],
+    children: [
       {
-        path :'',
-        component : BookListComponent
+        path: '',
+        component: BookListComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'my-books',
-        component:MyBooksComponent
-      },
-      {
-        path: 'manage',
-        component:ManageBookComponent
+        component: MyBooksComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'my-borrowed-books',
-        component:BorrowedBookListComponent
+        component: BorrowedBookListComponent,
+        canActivate: [authGuard]
+      },
+      {
+        path: 'my-returned-books',
+        component: ReturnedBooksComponent,
+        canActivate: [authGuard]
+      },
+
+      {
+        path: 'manage',
+        component: ManageBookComponent,
+        canActivate: [authGuard]
       },
       {
         path: 'manage/:bookId',
-        component:ManageBookComponent
-      },
-
+        component: ManageBookComponent,
+        canActivate: [authGuard]
+      }
     ]
   }
-
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class BookRoutingModule { }
+export class BookRoutingModule {
+}
